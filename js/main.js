e824524e7ca31b9535e2fd4bd495d457a9a8d8ea
Sergio Tanzilli <tanzilli@acmesystems.it>
2019-12-17 17:43:52 +0100

    Backup

--- js/main.js
+++ js/main.js
@@ -18,13 +18,33 @@ var randomString = function(length) {
 
 function onConnect() {
 	mqtt_mainpage_client.subscribe("tangowall/cmd");
-	console.log("Connesso");
 }	
 
 function onMessageArrived(message) {
 
 	console.log(message.payloadString);
 	json_data = JSON.parse(message.payloadString);
+
+	if (json_data.cmd=="tangowall_layer") {
+		$("#tangowall_layer").fadeIn();
+		$("#partywall_layer").fadeOut();
+		$("#mirror_2_layer").fadeOut();
+		return;
+	}
+
+	if (json_data.cmd=="partywall_layer") {
+		$("#tangowall_layer").fadeOut();
+		$("#partywall_layer").fadeIn();
+		$("#mirror_2_layer").fadeOut();
+		return;
+	}
+
+	if (json_data.cmd=="mirror_2_layer") {
+		$("#tangowall_layer").fadeOut();
+		$("#partywall_layer").fadeOut();
+		$("#mirror_2_layer").fadeIn();
+		return;
+	}
 
 	// Visibility
 	if (json_data.cmd=="visibility") {
@@ -37,6 +57,7 @@ function onMessageArrived(message) {
 		} else {
 			$("#" + json_data.element).fadeIn();
 		}
+		return;
 	}    
 
 	// Load video
@@ -46,6 +67,7 @@ function onMessageArrived(message) {
 
 		source_tag.src=json_data.filename;
 		video_tag.load();
+		return;
 	}    
 
 	// Play/PlaybackRate
@@ -53,11 +75,13 @@ function onMessageArrived(message) {
 		var element=document.getElementById(json_data.element); 
 		element.playbackRate=json_data.playbackRate;
 		element.play();
+		return;
 	}    
 
 	if (json_data.cmd=="pause") {
 		var element=document.getElementById(json_data.element); 
 		element.pause();
+		return;
 	}    
 
 	// currentTime
@@ -65,21 +89,78 @@ function onMessageArrived(message) {
 		var element=document.getElementById(json_data.element); 
 		
 		element.currentTime=json_data.current_time;
+		return;
 	}    
 
 	// seek
 	if (json_data.cmd=="seek") {
 		var element=document.getElementById(json_data.element); 
 		element.currentTime=element.currentTime+json_data.offset;
+		return;
 	}    
 
-	// Refresh della pagina
-	if (json_data.cmd=="loadpage") {
-		$("#all").fadeOut("slow",function() {
-			document.location.href = json_data.value;
+	
+
+	// Load video
+	if (json_data.cmd=="youtube_load") {
+		youtube_id = json_data.value;
+
+		player.on('ready', event => {
+			//const instance = event.detail.plyr;
+			player.play();
 		});
+
+
+		/*player.on('ended', event => {
+			var message;
+			console.log("Ended");
+			message = new Paho.MQTT.Message("end");
+			message.destinationName = "tangowall/finished";
+			mqtt_mainpage_client.send(message);
+		});*/
+		
+		player.on('statechange', event => {
+			if (event.detail.code===0) {
+				var message;
+				console.log(event.detail.code);
+				message = new Paho.MQTT.Message("end");
+				message.destinationName = "tangowall/finished";
+				mqtt_mainpage_client.send(message);
+			}
+		});
+
+
+		player.source = {
+		    type: 'video',
+		    sources: [
+		        {
+		            src: youtube_id,
+		            provider: 'youtube',
+		        },
+		    ],
+		};
+	}	
+		
+	if (json_data.cmd=="youtube_pause") {
+		player.pause();
+		return;
+	}
+	
+	if (json_data.cmd=="youtube_play") {
+		player.play();
+		return;
 	}
 
+
+
+
+	// Refresh della pagina
+	//if (json_data.cmd=="loadpage") {
+	//	$("#all").fadeOut("slow",function() {
+	//		document.location.href = json_data.value;
+	//	});
+	//	return;
+	//}
 
 	/* Seek
 	if (json_data.cmd=="seek") {
@@ -130,8 +211,16 @@ $(document).ready(function() {
 		onSuccess:onConnect
 	});
 
-	$("#video_slot").fadeIn();
+	$("#tangowall_layer").fadeIn();
 
+	// Change the second argument to your options:
+	// https://github.com/sampotts/plyr/#options
+	const player = new Plyr('#player', {captions: {active: true}});
+	
+	// Expose player so it can be used from the console
+	window.player = player;
+	
+	//player.play();
 	
 	//var elem = document.documentElement;
 	//openFullscreen(elem);
